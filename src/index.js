b3c6937851f104f99b05aebe410d86afe9167b07
Jaivignesh-afk <23f1001347@ds.study.iitm.ac.in>
2023-09-07 19:47:15 +0530

    App Component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './Components/App.jsx'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello World</h1>
+  <App />
 );
 
